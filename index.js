f564cecc582ff15ccb641bfdc4ee0177890370d9
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2022-02-16 19:36:48 +0530

    Added Navbar

---
+++ index.js
@@ -0,0 +1,10 @@
+// const menu = document.querySelector('#mobile_menu');
+// const menuLinks = document.querySelector('.navbar__menu');
+// // Display Mobile Menu :
+// const mobileMenu=()=>{
+//     menu.classList.toggle('is-active');
+//     menuLinks.classList.toggle('active');
+
+// }
+// menu.addEventListener('click',mobileMenu);
+// menuLinks.addEventListener('click',mobileMenu);
